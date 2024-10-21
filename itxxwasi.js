2c3587f6d56b1a02eda0acbbbe0251e0481273dc
𝚓𝚘𝚎𝚕 𝚓𝚊𝚖𝚎𝚜 <joeljamestech3@gmail.com>
2024-10-21 06:41:56 +0300

    𝙻𝚘𝚛𝚍 𝚓𝚘𝚎𝚕

---
+++ itxxwasi.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+__path = process.cwd()
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 8000;
+let server = require('./joelqr'),
+    code = require('./pair');
+require('events').EventEmitter.defaultMaxListeners = 500;
+app.use('/wasiqr', server);
+app.use('/code', code);
+app.use('/pair',async (req, res, next) => {
+res.sendFile(__path + '/pair.html')
+})
+app.use('/',async (req, res, next) => {
+res.sendFile(__path + '/joelpage.html')
+})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(PORT, () => {
+    console.log(`
+Dark love md bot
+
+ Server running on http://localhost:` + PORT)
+})
+
+module.exports = app
+/**
+    powered by wasi tech team 
+    join Whatsapp channel for more updates 
+    **/
