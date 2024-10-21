2a6f0d8b52939d460ddb3a1867fb6b7f5d5f34a9
𝚓𝚘𝚎𝚕 𝚓𝚊𝚖𝚎𝚜 <joeljamestech3@gmail.com>
2024-10-21 08:02:17 +0300

    𝙻𝚘𝚛𝚍 𝚓𝚘𝚎𝚕

--- itjoe.js
+++ itjoe.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 8000;
 let server = require('./joelqr'),
     code = require('./pair');
 require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/wasiqr', server);
+app.use('/joelqr', server);
 app.use('/code', code);
 app.use('/pair',async (req, res, next) => {
 res.sendFile(__path + '/pair.html')
